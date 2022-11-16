77f1a46e8f805cd4a49aa32de1ee6e51b9af6d89
choijinhyang <jinhyang5999@naver.com>
2022-11-16 22:54:49 +0900

    p64 테스트코드 테스트코드 메소드 실행시 오류수정완료 -> import 수정

--- src/test/java/com/gotohigh/springboot/web/HelloControllerTest.java
+++ src/test/java/com/gotohigh/springboot/web/HelloControllerTest.java
@@ -1,31 +1,28 @@
 package com.gotohigh.springboot.web;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;	// {1}
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
-    public void hello가_리턴된다() throws Exception{
+    public void hello_is_return() throws Exception{
         String hello = "hello";
-
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
 }
