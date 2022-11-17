3c9a8ef5d5565a939e160f686cae0d618f35db73
choijinhyang <jinhyang5999@naver.com>
2022-11-17 22:17:35 +0900

    p93 jpa build

--- src/test/java/com/gotohigh/springboot/web/HelloControllerTest.java
+++ src/test/java/com/gotohigh/springboot/web/HelloControllerTest.java
@@ -1,16 +1,15 @@
 package com.gotohigh.springboot.web;
 
-import org.junit.jupiter.api.Test;	// {1}
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ExtendWith(SpringExtension.class)
@@ -24,5 +23,15 @@ public class HelloControllerTest {
     public void hello_is_return() throws Exception{
         String hello = "hello";
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+    }
+
+    @Test
+    public void helloDto_is_return() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+        mvc.perform(get("/hello/dto").param("name", name).param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
